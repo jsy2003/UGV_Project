dc81e20356f4731c27fb89b083a161f608f9e137
jsy2003 <93853610+jsy2003@users.noreply.github.com>
2021-11-15 15:45:26 +0900

    Update BrushlessWheels.h

--- BrushlessDriveWheels_test/BrushlessWheels.h
+++ BrushlessDriveWheels_test/BrushlessWheels.h
@@ -1,1 +1,10 @@
+/**************************************************************************
+* A simple library to communicate and send command to Brushless Wheels UGV
+*/
+#ifndef BrushlessWheels_H
+#define BrushlessWheels_H
 
+
+
+
+#endif
